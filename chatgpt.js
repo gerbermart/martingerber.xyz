189be38f1d6b5a0d646a25b255ada3a8990b24ec
gerbermart <gerbermart@gmail.com>
2023-06-21 16:08:10 -0700

    vault rugged us

--- chatgpt.js
+++ chatgpt.js
@@ -1,23 +1,52 @@
-class ChatGPT {
-    constructor(apiKey) {
-        this.apiKey = apiKey;
-    }
+const chatLog = document.getElementById("chat-log");
+const chatInput = document.getElementById("chat-input");
+const sendBtn = document.getElementById("send-btn");
 
-    generateResponse(prompt) {
-        const openai = require("@openai/api");
-        const api = new openai(this.apiKey);
+const openai = new OpenAI("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");
 
-        const parameters = {
-            "model": "text-davinci-002",
-            "prompt": prompt,
-            "temperature": 0.7,
-            "max_tokens": 60,
-            "top_p": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0
-        };
-
-        return api.completions.create(parameters)
-            .then(response => response.data.choices[0].text);
-    }
+async function generateResponse(prompt) {
+  const completions = await openai.complete({
+    engine: "davinci",
+    prompt: prompt,
+    maxTokens: 150,
+    n: 1,
+    stop: ["\n"],
+    temperature: 0.7,
+  });
+  const message = completions.choices[0].text.trim();
+  return message;
 }
+
+function appendMessageToChatLog(message, sender) {
+  const messageNode = document.createElement("div");
+  messageNode.classList.add("chat-message");
+  messageNode.classList.add(sender === "user" ? "user-message" : "bot-message");
+  messageNode.innerText = message;
+  chatLog.appendChild(messageNode);
+}
+
+async function handleUserInput() {
+  const userInput = chatInput.value.trim();
+
+  if (userInput !== "") {
+    appendMessageToChatLog(userInput, "user");
+
+    const response = await generateResponse(userInput);
+    appendMessageToChatLog(response, "bot");
+
+    chatInput.value = "";
+  }
+}
+
+sendBtn.addEventListener("click", handleUserInput);
+chatInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    handleUserInput();
+  }
+});
+
+// Initial message from bot
+(async () => {
+  const response = await generateResponse("hello");
+  appendMessageToChatLog(response, "bot");
+})();
