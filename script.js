9d49626a0d5c65d41df53e0b0b27491e0d505d1c
gerbermart <gerbermart@gmail.com>
2023-06-25 07:27:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,405 @@
-const contractAddress = '<contract_address>'; // Replace with your contract address
+const contractAddress = '0xbb1696ed7CE36a50a947e20F3785DE11460AEcEe'; // Replace with the actual contract address
+const contractABI = [
+    [
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "addr",
+                    "type": "address"
+                }
+            ],
+            "name": "AddressAllowed",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "operator",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "from",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "to",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "id",
+                    "type": "uint256"
+                },
+                {
+                    "indexed": false,
+                    "internalType": "uint256",
+                    "name": "value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "TransferSingle",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "string",
+                    "name": "value",
+                    "type": "string"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "uint256",
+                    "name": "id",
+                    "type": "uint256"
+                }
+            ],
+            "name": "URI",
+            "type": "event"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "mintCount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "freeMint",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "mintCount",
+                    "type": "uint256"
+                }
+            ],
+            "name": "mintAlbum",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "uri_input",
+                    "type": "string"
+                }
+            ],
+            "name": "mintNewToken",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "addressInput",
+                    "type": "address"
+                }
+            ],
+            "name": "setAllow",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "newMintPrice",
+                    "type": "uint256"
+                }
+            ],
+            "name": "setMintPrice",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "address",
+                    "name": "addressInput",
+                    "type": "address"
+                }
+            ],
+            "name": "setPayout",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "uriInput",
+                    "type": "string"
+                }
+            ],
+            "name": "setTokenUri",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "uriInput",
+                    "type": "string"
+                }
+            ],
+            "name": "setURI",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "addresses",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "ALBUM_ID",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "allow_addresses",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "allowed_addresses",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "mintPrice",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "totalMints",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "tokenId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "uri",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+]; // Replace with the actual contract ABI
 
-// Function to update the message text
-function updateMessage(text) {
-  const messageElement = document.getElementById('message');
-  messageElement.innerText = text;
-}
+const ethereumProvider = window.ethereum;
 
-// Function to handle the button click event
 async function mintAlbum() {
+  const mintCount = 1; // Number of album mints
+  const mintPrice = await contract.methods.mintPrice().call();
+  const value = ethereumProvider.utils.toWei(String(mintPrice * mintCount));
+
   try {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const accounts = await ethereumProvider.request({ method: 'eth_requestAccounts' });
     const from = accounts[0];
-    const tokenId = 1;
-    const mintCount = 1;
 
-    // Call the contract's mintAlbum function
-    const response = await fetch(`https://api.infura.io/v1/jsonrpc/mainnet/eth_call?params=[{"to": "${contractAddress}", "data": "0xd5a54fbf000000000000000000000000000000000000000000000000000000000000001"}]`);
-    const result = await response.json();
+    const transactionParameters = {
+      from: from,
+      to: contractAddress,
+      value: value,
+      data: contract.methods.mintAlbum(ALBUM_ID, mintCount).encodeABI()
+    };
 
-    if (result.error) {
-      updateMessage('Minting failed. Please try again.');
-    } else {
-      // Check if the transaction was successful
-      if (result.result === '0x') {
-        updateMessage('Minting in progress...');
-      } else {
-        updateMessage('MINT COMPLETE!');
-      }
-    }
+    const txHash = await ethereumProvider.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters]
+    });
+
+    console.log('Mint Transaction Hash:', txHash);
   } catch (error) {
-    console.error(error);
-    updateMessage('An error occurred. Please try again.');
+    console.log('Mint Failed:', error);
   }
 }
+
+async function freeMint() {
+  const mintCount = 1; // Number of album mints
+
+  try {
+    const accounts = await ethereumProvider.request({ method: 'eth_requestAccounts' });
+    const from = accounts[0];
+
+    const transactionParameters = {
+      from: from,
+      to: contractAddress,
+      data: contract.methods.freeMint(ALBUM_ID, mintCount).encodeABI()
+    };
+
+    const txHash = await ethereumProvider.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters]
+    });
+
+    console.log('Mint Transaction Hash:', txHash);
+  } catch (error) {
+    console.log('Mint Failed:', error);
+  }
+}
+
+async function initializeContract() {
+  try {
+    const accounts = await ethereumProvider.request({ method: 'eth_requestAccounts' });
+    const from = accounts[0];
+
+    const web3 = new Web3(ethereumProvider);
+    contract = new web3.eth.Contract(contractABI, contractAddress, { from: from });
+    console.log('Contract Initialized');
+  } catch (error) {
+    console.log('Contract Initialization Failed:', error);
+  }
+}
+
+// Initialize the contract and enable minting buttons
+initializeContract();
